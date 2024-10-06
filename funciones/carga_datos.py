202f8cf0ffffdbaffb8b0bc011d1587d5e981a5f
mito-droide <tomimart96@gmail.com>
2024-10-06 20:57:00 -0300

    readme

--- funciones/carga_datos.py
+++ funciones/carga_datos.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 # Cargar los textos de análisis desde el archivo Excel
 def cargar_textos():
-    textos = pd.read_excel("data/textos_analisis.xlsx")
+    textos = pd.read_excel("textos_analisis.xlsx")
     return textos
 
 # Simulación de un dataframe con los datos electorales por facultad
 def cargar_datos_electorales():
-    datos = pd.read_excel('data/uba_cd_estudiantes.xlsx', sheet_name='base')
+    datos = pd.read_excel('uba_cd_estudiantes.xlsx', sheet_name='base')
     return datos
